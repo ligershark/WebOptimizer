516bd76e3ee149cd30d3c900f9b078c833fcd80a
Mads Kristensen <post@madskristensen.net>
2017-07-27 21:51:02 -0700

    Added unit test project Fixed #18

---
+++ test/Bundler.Test/Processors/CssMinifierTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Bundler.Processors;
+using NUglify.Css;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Http;
+
+namespace Bundler.Test.Processors
+{
+    public class CssMinifierTest
+    {
+        [Fact]
+        public void ExecuteTest_Success()
+        {
+            var minifier = new CssMinifier(new CssSettings());
+            var context = new Mock<IAssetContext>().SetupAllProperties();
+            context.Object.Content = "body { color: yellow; }";
+
+            minifier.Execute(context.Object);
+
+            Assert.Equal("body{color:#ff0}", context.Object.Content);
+            Assert.Equal("", minifier.CacheKey(new DefaultHttpContext()));
+        }
+    }
+}
