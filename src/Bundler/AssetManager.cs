432f263748725b9fe21a6a3cb9e5acdf57d9a76e
Mads Kristensen <post@madskristensen.net>
2017-07-28 15:31:54 -0700

    Cleanup [skip ci]

--- src/Bundler/AssetManager.cs
+++ src/Bundler/AssetManager.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 
 namespace Bundler
 {
@@ -13,7 +12,7 @@ namespace Bundler
     public static class AssetManager
     {
         /// <summary>
-        /// Gets the builder associated with the pipeline.
+        /// This is only used by the Localizer. Would be great not to have it here if possible
         /// </summary>
         internal static IApplicationBuilder Builder { get; private set; }
 
@@ -21,9 +20,9 @@ namespace Bundler
         /// Gets the asset pipeline configuration
         /// </summary>
         public static IAssetPipeline Pipeline { get; private set; }
-        
+
         /// <summary>
-        /// Adds Bundler to the <see cref="IApplicationBuilder"/> request execution pipeline
+        /// Adds WebOptimizer to the <see cref="IApplicationBuilder"/> request execution pipeline
         /// </summary>
         public static void UseWebOptimizer(this IApplicationBuilder app, IHostingEnvironment env, Action<IAssetPipeline> assetPipeline)
         {
