ligershark/WebOptimizer

commit e3e169e9f35b9b169f835ca819ca46d074cdc8a9

Multiple providers and dynamic configuration (#76)
* .

* cleanup

src/WebOptimizer.Core/Asset.cs
@@ -87,23 +87,36 @@ namespace WebOptimizer
 
         public static IEnumerable<string> ExpandGlobs(IAsset asset, IHostingEnvironment env)
         {
-            string root = asset.GetFileProvider(env).GetFileInfo("/").PhysicalPath;
-            var dir = new DirectoryInfoWrapper(new DirectoryInfo(root));
             var files = new List<string>();
 
             foreach (string sourceFile in asset.SourceFiles)
             {
-                var matcher = new Matcher();
-                matcher.AddInclude(sourceFile);
-                PatternMatchingResult globbingResult = matcher.Execute(dir);
-                IEnumerable<string> fileMatches = globbingResult.Files.Select(f => f.Path.Replace(root, string.Empty));
+                string outSourceFile;
+                var provider = asset.GetFileProvider(env, sourceFile, out outSourceFile);
 
-                if (!fileMatches.Any())
+                var fileInfo = provider.GetFileInfo("/");
+                string root = fileInfo.PhysicalPath;
+                if (root != null)
                 {
-                    throw new FileNotFoundException($"No files found matching \"{sourceFile}\" exist in \"{dir.FullName}\"");
+                    var dir = new DirectoryInfoWrapper(new DirectoryInfo(root));
+                    var matcher = new Matcher();
+                    matcher.AddInclude(outSourceFile);
+                    PatternMatchingResult globbingResult = matcher.Execute(dir);
+                    IEnumerable<string> fileMatches = globbingResult.Files.Select(f => f.Path.Replace(root, string.Empty));
+
+                    if (!fileMatches.Any())
+                    {
+                        throw new FileNotFoundException($"No files found matching \"{sourceFile}\" exist in \"{dir.FullName}\"");
+                    }
+                    //files.AddRange(fileMatches.Where(f => !files.Contains(f)));
+                    files.Add(sourceFile);
+                }
+                else
+                {
+                    files.Add(sourceFile);
                 }
 
-                files.AddRange(fileMatches.Where(f => !files.Contains(f)));
+
             }
 
             asset.Items[PhysicalFilesKey] = files;
@@ -115,7 +128,7 @@ namespace WebOptimizer
         {
             IFileInfo file = fileProvider.GetFileInfo(sourceFile);
 
-            using (Stream fs = File.OpenRead(file.PhysicalPath))
+            using (Stream fs = file.CreateReadStream())
             {
                 byte[] bytes = await fs.AsBytesAsync();
 
@@ -218,6 +231,22 @@ namespace WebOptimizer
         public static IFileProvider GetFileProvider(this IAsset asset, IHostingEnvironment env)
         {
             return asset.GetCustomFileProvider(env) ?? env.WebRootFileProvider;
+        }
+
+        /// <summary>
+        /// Gets the file provider.
+        /// </summary>
+        internal static IFileProvider GetFileProvider(this IAsset asset, IHostingEnvironment env, string path, out string outpath)
+        {
+            var provider = asset.GetCustomFileProvider(env) ?? env.WebRootFileProvider;
+
+            if (provider is CompositeFileProviderExtended)
+            {
+                return ((CompositeFileProviderExtended)provider).GetFileProvider(path, out outpath);
+            }
+
+            outpath = path;
+            return provider;
         }
     }
 }

WebOptimizer.sln
@@ -16,6 +16,10 @@ Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "WebOptimizer.Core.Test", "t
 EndProject
 Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "WebOptimizer.Core.Sample", "sample\WebOptimizer.Core.Sample.csproj", "{478357DE-07B8-443D-B430-E2390F74B060}"
 EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "WebOptimizer.Core.Sample2", "sample2\WebOptimizer.Core.Sample2\WebOptimizer.Core.Sample2.csproj", "{885AD8B7-1843-4579-8C28-E127B4A7CF68}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "WebOptimizer.Core.Sample2.Lib", "sample2\WebOptimizer.Core.Sample2.Lib\WebOptimizer.Core.Sample2.Lib.csproj", "{F93B1ADA-6BF1-4BA3-9E88-2C5D84D81AD8}"
+EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
 		Debug|Any CPU = Debug|Any CPU
@@ -34,6 +38,14 @@ Global
 		{478357DE-07B8-443D-B430-E2390F74B060}.Debug|Any CPU.Build.0 = Debug|Any CPU
 		{478357DE-07B8-443D-B430-E2390F74B060}.Release|Any CPU.ActiveCfg = Release|Any CPU
 		{478357DE-07B8-443D-B430-E2390F74B060}.Release|Any CPU.Build.0 = Release|Any CPU
+		{885AD8B7-1843-4579-8C28-E127B4A7CF68}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{885AD8B7-1843-4579-8C28-E127B4A7CF68}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{885AD8B7-1843-4579-8C28-E127B4A7CF68}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{885AD8B7-1843-4579-8C28-E127B4A7CF68}.Release|Any CPU.Build.0 = Release|Any CPU
+		{F93B1ADA-6BF1-4BA3-9E88-2C5D84D81AD8}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{F93B1ADA-6BF1-4BA3-9E88-2C5D84D81AD8}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{F93B1ADA-6BF1-4BA3-9E88-2C5D84D81AD8}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{F93B1ADA-6BF1-4BA3-9E88-2C5D84D81AD8}.Release|Any CPU.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE

sample2/WebOptimizer.Core.Sample2.Lib/AssemblyTools.cs
@@ -1,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace WebOptimizer.Core.Sample2.Lib
+{
+    public class AssemblyTools
+    {
+        public static Assembly GetCurrentAssembly()
+        {
+            return typeof(AssemblyTools).Assembly;
+        }
+    }
+}

sample2/WebOptimizer.Core.Sample2.Lib/EmbeddedScript1.js
@@ -1,0 +1,7 @@
+﻿(function (innerhtml) {
+    var content = document.getElementById("content"),
+        div = document.createElement("div");
+
+    div.innerHTML = innerhtml;
+    content.appendChild(div);
+})('/EmbeddedResourcesScripts/EmbeddedScript1.js loaded');

sample2/WebOptimizer.Core.Sample2.Lib/Scripts/EmbeddedScript2.js
@@ -1,0 +1,7 @@
+﻿(function (innerhtml) {
+    var content = document.getElementById("content"),
+        div = document.createElement("div");
+
+    div.innerHTML = innerhtml;
+    content.appendChild(div);
+})('/EmbeddedResourcesScripts/Scripts/EmbeddedScript2.js loaded');

sample2/WebOptimizer.Core.Sample2.Lib/WebOptimizer.Core.Sample2.Lib.csproj
@@ -1,0 +1,18 @@
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+
+
+	<ItemGroup>
+		<None Remove="Scripts\EmbeddedScript2.js" />
+		<None Remove="EmbeddedScript1.js" />
+	</ItemGroup>
+
+	<ItemGroup>
+		<EmbeddedResource Include="Scripts\EmbeddedScript2.js" />
+		<EmbeddedResource Include="EmbeddedScript1.js" />
+	</ItemGroup>
+
+</Project>

sample2/WebOptimizer.Core.Sample2/Controllers/HomeController.cs
@@ -1,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebOptimizer.Core.Sample2.Models;
+
+namespace WebOptimizer.Core.Sample2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

sample2/WebOptimizer.Core.Sample2/Models/ErrorViewModel.cs
@@ -1,0 +1,11 @@
+using System;
+
+namespace WebOptimizer.Core.Sample2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

commit 882128b8de4c44a27d5e3ccce9573d686ed2e127

Fixes #79 Asset.ExpandGlobs got broken in PR #76

src/WebOptimizer.Core/Asset.cs
@@ -108,8 +108,7 @@ namespace WebOptimizer
                     {
                         throw new FileNotFoundException($"No files found matching \"{sourceFile}\" exist in \"{dir.FullName}\"");
                     }
-                    //files.AddRange(fileMatches.Where(f => !files.Contains(f)));
-                    files.Add(sourceFile);
+                    files.AddRange(fileMatches.Where(f => !files.Contains(f)));
                 }
                 else
                 {