cd44f7cbff7a45ed4555ba2a9412f5713f86ffbb
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-05-19 08:54:59 -0700

    Disposing lingering object to fix memory leak [release] Fixes #170

--- src/WebOptimizer.Core/WebOptimizerConfig.cs
+++ src/WebOptimizer.Core/WebOptimizerConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -6,11 +8,13 @@ using Microsoft.Extensions.Options;
 
 namespace WebOptimizer
 {
-    internal class WebOptimizerConfig : IConfigureOptions<WebOptimizerOptions>
+    internal class WebOptimizerConfig : IConfigureOptions<WebOptimizerOptions>, IDisposable
     {
-        private IConfiguration _config;
-        private IOptionsMonitorCache<WebOptimizerOptions> _options;
+        private readonly IConfiguration _config;
+        private readonly IOptionsMonitorCache<WebOptimizerOptions> _options;
         private readonly IWebHostEnvironment _hostingEnvironment;
+        private IDisposable _callback;
+        private bool _disposedValue;
 
         public WebOptimizerConfig(IConfiguration config, IOptionsMonitorCache<WebOptimizerOptions> options, IWebHostEnvironment hostingEnvironment)
         {
@@ -21,21 +25,41 @@ namespace WebOptimizer
 
         public void Configure(WebOptimizerOptions options)
         {
-            _config.GetReloadToken().RegisterChangeCallback(_ =>
+            _callback = _config.GetReloadToken().RegisterChangeCallback(_ =>
             {
                 _options.TryRemove(Options.DefaultName);
             }, null);
 
             _config.GetSection("WebOptimizer").Bind(options);
 
-            options.EnableCaching = options.EnableCaching ?? !_hostingEnvironment.IsDevelopment();
-            options.EnableDiskCache = options.EnableDiskCache ?? !_hostingEnvironment.IsDevelopment();
-            options.EnableMemoryCache = options.EnableMemoryCache ?? true;
-            options.EnableTagHelperBundling = options.EnableTagHelperBundling ?? true;
+            options.EnableCaching ??= !_hostingEnvironment.IsDevelopment();
+            options.EnableDiskCache ??= !_hostingEnvironment.IsDevelopment();
+            options.EnableMemoryCache ??= true;
+            options.EnableTagHelperBundling ??= true;
             options.CacheDirectory = string.IsNullOrWhiteSpace(options.CacheDirectory)
                 ? Path.Combine(_hostingEnvironment.ContentRootPath, "obj", "WebOptimizerCache")
                 : options.CacheDirectory;
-            options.AllowEmptyBundle = options.AllowEmptyBundle ?? false;
+            options.AllowEmptyBundle ??= false;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _callback?.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
