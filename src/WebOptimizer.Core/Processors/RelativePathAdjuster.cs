ligershark/WebOptimizer

commit bf0933112a3ede2077bc9df4fe4a6f757b1daa7e

Full support if app uses IApplicationBuilder.UsePathBase (#320)
[release]

src/WebOptimizer.Core/Processors/RelativePathAdjuster.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.FileProviders;
 using WebOptimizer;
 using WebOptimizer.Utils;
 
@@ -17,8 +15,6 @@ namespace WebOptimizer
         public override Task ExecuteAsync(IAssetContext config)
         {
             var content = new Dictionary<string, byte[]>();
-            var env = (IWebHostEnvironment)config.HttpContext.RequestServices.GetService(typeof(IWebHostEnvironment));
-            IFileProvider fileProvider = config.Asset.GetAssetFileProvider(env);
 
             foreach (string key in config.Content.Keys)
             {
@@ -59,12 +55,7 @@ namespace WebOptimizer
                     ? config.HttpContext.Request.PathBase.Value
                     : "/";
 
-                string routeRelativePath =
-                    config.Asset.Route.StartsWith("~/")
-                    ? config.Asset.Route.Substring(2)
-                    : config.Asset.Route;
-
-                string routePath = UrlPathUtils.MakeAbsolute(appPath, routeRelativePath);
+                string routePath = UrlPathUtils.MakeAbsolute(appPath, config.Asset.Route.TrimStart('/'));
 
                 // prevent query string from causing error
                 string[] pathAndQuery = urlValue.Split(new[] { '?' }, 2, StringSplitOptions.RemoveEmptyEntries);

src/WebOptimizer.Core/Processors/CssFingerprinter.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Primitives;
 using WebOptimizer;
 using WebOptimizer.Utils;
 
@@ -55,19 +52,7 @@ namespace WebOptimizer
                 if (urlValue.StartsWith("//"))
                     return match.Value;
 
-                // get absolute path of content file
-                string appPath = (config.HttpContext?.Request?.PathBase.HasValue ?? false)
-                    ? config.HttpContext.Request.PathBase.Value
-                    : "/";
-
-                string routeRelativePath =
-                    config.Asset.Route.StartsWith("~/")
-                        ? config.Asset.Route.Substring(2)
-                        : config.Asset.Route;
-
-                string routePath = UrlPathUtils.MakeAbsolute(appPath, routeRelativePath);
-
-                string routeBasePath = UrlPathUtils.GetDirectory(routePath);
+                string routeBasePath = UrlPathUtils.GetDirectory(config.Asset.Route);
 
                 // prevent query string from causing error
                 string[] pathAndQuery = urlValue.Split(new[] { '?' }, 2, StringSplitOptions.RemoveEmptyEntries);
@@ -75,7 +60,7 @@ namespace WebOptimizer
                 string queryOnly = pathAndQuery.Length == 2 ? pathAndQuery[1] : string.Empty;
 
                 // get filepath of included file
-                if (!UrlPathUtils.TryMakeAbsolutePathFromInclude(appPath, routeBasePath, pathOnly, out string filePath))
+                if (!UrlPathUtils.TryMakeAbsolute(routeBasePath, pathOnly, out string filePath))
                     // path to included file is invalid
                     return match.Value;
 

src/WebOptimizer.Core/Processors/CssImageInliner.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.FileProviders;
 using WebOptimizer;
@@ -73,19 +72,7 @@ namespace WebOptimizer
             if (urlValue.StartsWith("//"))
                 return match.Value;
 
-            // get absolute path of content file
-            string appPath = (config.HttpContext?.Request?.PathBase.HasValue ?? false)
-                ? config.HttpContext.Request.PathBase.Value
-                : "/";
-
-            string routeRelativePath =
-                config.Asset.Route.StartsWith("~/")
-                    ? config.Asset.Route.Substring(2)
-                    : config.Asset.Route;
-
-            string routePath = UrlPathUtils.MakeAbsolute(appPath, routeRelativePath);
-
-            string routeBasePath = UrlPathUtils.GetDirectory(routePath);
+            string routeBasePath = UrlPathUtils.GetDirectory(config.Asset.Route);
 
             // prevent query string from causing error
             string[] pathAndQuery = urlValue.Split(new[] { '?' }, 2, StringSplitOptions.RemoveEmptyEntries);
@@ -93,7 +80,7 @@ namespace WebOptimizer
             string queryOnly = pathAndQuery.Length == 2 ? pathAndQuery[1] : string.Empty;
 
             // get filepath of included file
-            if (!UrlPathUtils.TryMakeAbsolutePathFromInclude(appPath, routeBasePath, pathOnly, out string filePath))
+            if (!UrlPathUtils.TryMakeAbsolute(routeBasePath, pathOnly, out string filePath))
                 // path to included file is invalid
                 return match.Value;
 

test/WebOptimizer.Core.Test/Processors/CssFinterprinterTest.cs
@@ -37,13 +37,16 @@ namespace WebOptimizer.Test.Processors
             var fileProvider = new Mock<IFileProvider>();
 
             context.SetupGet(s => s.Asset.Route)
-                .Returns("~/css/site.css");
+                .Returns(AssetPipeline.NormalizeRoute("~/css/site.css"));
 
             context.Setup(s => s.HttpContext.RequestServices.GetService(typeof(IAssetPipeline)))
                 .Returns(pipeline.Object);
 
             context.Setup(s => s.HttpContext.RequestServices.GetService(typeof(IWebHostEnvironment)))
                 .Returns(env.Object);
+
+            context.Setup(s => s.HttpContext.Request.PathBase)
+                .Returns("/parent");
 
             env.SetupGet(e => e.WebRootFileProvider)
                 .Returns(fileProvider.Object);

test/WebOptimizer.Core.Test/Processors/CssImageInlinerTest.cs
@@ -57,6 +57,9 @@ namespace WebOptimizer.Test.Processors
             context.Setup(s => s.HttpContext.RequestServices.GetService(typeof(IWebHostEnvironment)))
                 .Returns(env.Object);
 
+            context.Setup(s => s.HttpContext.Request.PathBase)
+                .Returns("/parent");
+
             env.SetupGet(e => e.WebRootFileProvider)
                 .Returns(fileProvider.Object);
 

test/WebOptimizer.Core.Test/Processors/RelativePathAdjusterTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,26 +14,24 @@ namespace WebOptimizer.Test.Processors
     public class RelativePathAdjusterTest
     {
         [Theory2]
-        [InlineData("/dist/all.css", "css/site.css", "url(/img/foo.png)", "url(/img/foo.png)")]
-        [InlineData("/dist/all.css", "css/site.css", "url(/img/foo.png?1=1)", "url(/img/foo.png?1=1)")]
-        [InlineData("/dist/all.css", "css/site.css", "url(img/foo.png)", "url(../css/img/foo.png)")]
-        [InlineData("/dist/all.css", "css/site.css", "url(http://foo.png)", "url(http://foo.png)")]
-        [InlineData("/dist/all.css", "css/site.css", "url('img/foo.png')", "url('../css/img/foo.png')")]
-        [InlineData("/dist/all.css", "css/site.css", "url(\"img/foo.png\")", "url(\"../css/img/foo.png\")")]
-        [InlineData("/dist/all.css", "css/sub/site.css", "url(img/foo.png)", "url(../css/sub/img/foo.png)")]
-        [InlineData("/css/all.css", "css/site.css", "url(img/foo.png)", "url(img/foo.png)")]
-        [InlineData("/css/all.css", "css/site.css", "url(../img/foo.png)", "url(../img/foo.png)")]
-        [InlineData("/css/all.css", "css/site.css", "url(../../img/foo.png)", "url(../../img/foo.png)")]
-        [InlineData("/dist/sub/all.css", "css/sub/site.css", "url(img/foo.png)", "url(../../css/sub/img/foo.png)")]
-        [InlineData("/dist/all.css", "css/site.css", "url(../img/foo.png)", "url(../img/foo.png)")]
-        [InlineData("dist/all.css", "css/site.css", "url(img/foo.png)", "url(../css/img/foo.png)")]
-        [InlineData("dist/all.css", "css/site.css", "url(../img/foo.png)", "url(../img/foo.png)")]
-        [InlineData("/css/all.css", "css/site.css", "url(~/img/foo.png)", "url(../img/foo.png)")]
-        [InlineData("/css/all.css", "css/sub/site.css", "url(~/img/foo.png)", "url(../img/foo.png)")]
-        [InlineData("/dist/sub/all.css", "css/sub/site.css", "url(~/img/foo.png)", "url(../../img/foo.png)")]
-        [InlineData("/dist/sub/all.css", "css/site.css", "url(~/img/foo.png)", "url(../../img/foo.png)")]
-        [InlineData("dist/sub/all.css", "css/site.css", "url(~/img/foo.png)", "url(../../img/foo.png)")]
-        public async Task AdjustRelativePaths_Success(string route, string inputPath, string url, string newUrl)
+        [InlineData(null, "/dist/all.css", "css/site.css", "url(/img/foo.png)", "url(/img/foo.png)")]
+        [InlineData(null, "/dist/all.css", "css/site.css", "url(/img/foo.png?1=1)", "url(/img/foo.png?1=1)")]
+        [InlineData(null, "/dist/all.css", "css/site.css", "url(img/foo.png)", "url(../css/img/foo.png)")]
+        [InlineData(null, "/dist/all.css", "css/site.css", "url(http://foo.png)", "url(http://foo.png)")]
+        [InlineData(null, "/dist/all.css", "css/site.css", "url('img/foo.png')", "url('../css/img/foo.png')")]
+        [InlineData(null, "/dist/all.css", "css/site.css", "url(\"img/foo.png\")", "url(\"../css/img/foo.png\")")]
+        [InlineData(null, "/dist/all.css", "css/sub/site.css", "url(img/foo.png)", "url(../css/sub/img/foo.png)")]
+        [InlineData(null, "/css/all.css", "css/site.css", "url(img/foo.png)", "url(img/foo.png)")]
+        [InlineData(null, "/css/all.css", "css/site.css", "url(../img/foo.png)", "url(../img/foo.png)")]
+        [InlineData(null, "/css/all.css", "css/site.css", "url(../../img/foo.png)", "url(../../img/foo.png)")]
+        [InlineData(null, "/dist/sub/all.css", "css/sub/site.css", "url(img/foo.png)", "url(../../css/sub/img/foo.png)")]
+        [InlineData(null, "/dist/all.css", "css/site.css", "url(../img/foo.png)", "url(../img/foo.png)")]
+        [InlineData(null, "dist/all.css", "css/site.css", "url(img/foo.png)", "url(../css/img/foo.png)")]
+        [InlineData(null, "dist/all.css", "css/site.css", "url(../img/foo.png)", "url(../img/foo.png)")]
+        [InlineData("/parent", "/css/all.css", "css/site.css", "url(../img/foo.png)", "url(../img/foo.png)")]
+        [InlineData("/parent", "/css/all.css", "css/site.css", "url(img/foo.png)", "url(img/foo.png)")]
+        [InlineData("/parent", "/dist/all.css", "css/site.css", "url(/img/foo.png)", "url(/img/foo.png)")]
+        public async Task AdjustRelativePaths_Success(string? requestPathBase, string route, string inputPath, string url, string newUrl)
         {
             var adjuster = new RelativePathAdjuster();
             var context = new Mock<IAssetContext>().SetupAllProperties();
@@ -51,6 +50,12 @@ namespace WebOptimizer.Test.Processors
 
             context.Setup(s => s.HttpContext.RequestServices.GetService(typeof(IWebHostEnvironment)))
                    .Returns(env.Object);
+
+            if (requestPathBase != null)
+            {
+                context.Setup(s => s.HttpContext.Request.PathBase)
+                    .Returns(requestPathBase);
+            }
 
             env.SetupGet(e => e.WebRootFileProvider)
                  .Returns(fileProvider.Object);

commit 1e3988741ede74e1f35f547ef113a156c57dc990

Fixes issues caused by commit bf0933112a3ede2077bc9df4fe4a6f757b1daa7e when generating relative urls for resources inside css-files. (#325).

src/WebOptimizer.Core/Processors/RelativePathAdjuster.cs
@@ -55,7 +55,7 @@ namespace WebOptimizer
                     ? config.HttpContext.Request.PathBase.Value
                     : "/";
 
-                string routePath = UrlPathUtils.MakeAbsolute(appPath, config.Asset.Route.TrimStart('/'));
+                string routePath = UrlPathUtils.MakeAbsolute(appPath, config.Asset.Route);
 
                 // prevent query string from causing error
                 string[] pathAndQuery = urlValue.Split(new[] { '?' }, 2, StringSplitOptions.RemoveEmptyEntries);