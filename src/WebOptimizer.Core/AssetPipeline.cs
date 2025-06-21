2b3a37c6f1f0e62311aaf2a6d85701adebea245d
Bill Forney <william@williamforney.com>
2025-06-20 22:14:53 -0700

    Enhance AssetPipeline with documentation and refactoring
    Updated XML documentation for `_assetLogger` and other methods.
    Refactored `NormalizeRoute` and `TryGetAssetFromRoute` for improved functionality and readability.
    Ensured correct asset addition in `AddAsset` method.
    Overall code structure improved for better readability and consistency.

--- src/WebOptimizer.Core/AssetPipeline.cs
+++ src/WebOptimizer.Core/AssetPipeline.cs
@@ -7,17 +7,153 @@ namespace WebOptimizer;
 
 internal class AssetPipeline(ILogger<Asset> assetLogger) : IAssetPipeline
 {
+    /// <remarks>
+    /// For use by the Asset constructor only. Do not use for logging messages inside <see cref="AssetPipeline"/>.
+    /// </remarks>
+    internal ILogger<Asset> _assetLogger = assetLogger ?? throw new ArgumentNullException(nameof(assetLogger));
+
     internal ConcurrentDictionary<string, IAsset> _assets = new(StringComparer.OrdinalIgnoreCase);
 
     /// <summary>
-    /// For use by the Asset constructor only. Do not use for logging messages inside <see cref="AssetPipeline"/>.
+    /// Gets the assets registered on the pipeline.
     /// </summary>
-    internal ILogger<Asset> _assetLogger = assetLogger ?? throw new ArgumentNullException(nameof(assetLogger));
-
+    /// <value>The assets.</value>
     public IReadOnlyList<IAsset> Assets => [.. _assets.Values];
 
     /// <summary>
-    /// Gets the <see cref="IAsset" /> from the specified route.
+    /// Normalizes the route.
+    /// </summary>
+    /// <param name="route">The route.</param>
+    /// <returns>System.String.</returns>
+    public static string NormalizeRoute(string route)
+    {
+        string trimmedRoute = route.Trim();
+        string cleanRoute = trimmedRoute.StartsWith('/') || trimmedRoute.StartsWith('~')
+            ? $"/{route.Trim().TrimStart('~', '/')}"
+            : trimmedRoute;
+
+        int index = cleanRoute.IndexOfAny(['?', '#']);
+
+        if (index > -1)
+        {
+            cleanRoute = cleanRoute[..index];
+        }
+
+        if (!cleanRoute.StartsWith('/'))
+        {
+            cleanRoute = $"/{cleanRoute}";
+        }
+
+        return cleanRoute;
+    }
+
+    /// <summary>
+    /// Add a generalized Asset, that has just a Route and a ContentType
+    /// Typically used to fill in with content details later by Processors
+    /// Used by AddJavaScriptBundle() to emit Source Maps on their own route
+    /// </summary>
+    /// <param name="route">The route that should cause the pipeline to respond with this Asset</param>
+    /// <param name="contentType">Content-Type of the response</param>
+    /// <returns>IAsset.</returns>
+    public IAsset AddAsset(string route, string contentType)
+    {
+        route = NormalizeRoute(route);
+
+        IAsset asset = new Asset(route, contentType, [], _assetLogger);
+        _ = _assets.TryAdd(route, asset);
+
+        return asset;
+    }
+
+    /// <summary>
+    /// Adds an <see cref="IAsset" /> to the optimization pipeline.
+    /// </summary>
+    /// <param name="asset">The asset.</param>
+    /// <returns>IAsset.</returns>
+    public IAsset AddBundle(IAsset asset) =>
+            AddBundle(asset.Route, asset.ContentType, [.. asset.SourceFiles]);
+
+    /// <summary>
+    /// Adds an enumerable of <see cref="IAsset" /> to the optimization pipeline.
+    /// </summary>
+    /// <param name="assets">The assets.</param>
+    /// <returns>IEnumerable&lt;IAsset&gt;.</returns>
+    public IEnumerable<IAsset> AddBundle(IEnumerable<IAsset> assets)
+    {
+        var list = new List<IAsset>();
+
+        foreach (var asset in assets)
+        {
+            var ass = AddBundle(asset.Route, asset.ContentType, [.. asset.SourceFiles]);
+            list.Add(ass);
+        }
+
+        return list;
+    }
+
+    /// <summary>
+    /// Adds an asset to the optimization pipeline.
+    /// </summary>
+    /// <param name="route">The route matching for the asset.</param>
+    /// <param name="contentType">The content type of the response. Example: "text/css".</param>
+    /// <param name="sourceFiles">A list of relative file names of the sources to optimize.</param>
+    /// <returns>IAsset.</returns>
+    /// <exception cref="ArgumentException">At least one source file has to be specified - sourceFiles</exception>
+    /// <exception cref="ArgumentException">The route \"{route}\" appears to be a globbing pattern which isn't supported for bundle routes. - route</exception>
+    public IAsset AddBundle(string route, string contentType, params string[] sourceFiles)
+    {
+        if (sourceFiles.Length == 0)
+        {
+            throw new ArgumentException("At least one source file has to be specified", nameof(sourceFiles));
+        }
+
+        if (route.Contains('*') || (route.Contains('[') && route.Contains('?')))
+        {
+            throw new ArgumentException($"The route \"{route}\" appears to be a globbing pattern which isn't supported for bundle routes.", nameof(route));
+        }
+
+        route = NormalizeRoute(route);
+
+        IAsset asset = new Asset(route, contentType, sourceFiles, _assetLogger);
+        _ = _assets.TryAdd(route, asset);
+
+        return asset;
+    }
+
+    /// <summary>
+    /// Adds an array of files to the optimization pipeline.
+    /// </summary>
+    /// <param name="contentType">The content type of the response. Example: text/css or text/javascript.</param>
+    /// <param name="sourceFiles">A list of relative file names or globbing patterns of the sources to add.</param>
+    /// <returns>IEnumerable&lt;IAsset&gt;.</returns>
+    /// <exception cref="ArgumentException">A valid content type must be specified - contentType</exception>
+    /// <exception cref="ArgumentException">At least one source file has to be specified - sourceFiles</exception>
+    public IEnumerable<IAsset> AddFiles(string contentType, params string[] sourceFiles)
+    {
+        if (string.IsNullOrEmpty(contentType))
+        {
+            throw new ArgumentException("A valid content type must be specified", nameof(contentType));
+        }
+
+        if (sourceFiles.Length == 0)
+        {
+            throw new ArgumentException("At least one source file has to be specified", nameof(sourceFiles));
+        }
+
+        var list = new List<IAsset>();
+
+        foreach (string file in sourceFiles)
+        {
+            var asset = new Asset(NormalizeRoute(file), contentType, [file], _assetLogger);
+            list.Add(asset);
+            _ = _assets.TryAdd(asset.Route, asset);
+        }
+
+        return list;
+    }
+
+    /// <summary>
+    /// Gets the <see cref="IAsset"/> from the specified route.
     /// </summary>
     /// <param name="route">The route to find the asset by.</param>
     /// <param name="asset">The asset matching the route.</param>
@@ -50,7 +186,7 @@ internal class AssetPipeline(ILogger<Asset> assetLogger) : IAssetPipeline
                 try
                 {
                     var matcher = new Matcher();
-                    matcher.AddInclude(existing.Route);
+                    _ = matcher.AddInclude(existing.Route);
                     result = matcher.Match(cleanRoute.TrimStart('/'));
                 }
                 catch (Exception ex)
@@ -83,105 +219,12 @@ internal class AssetPipeline(ILogger<Asset> assetLogger) : IAssetPipeline
                         asset.Items.Add(items);
                     }
 
-                    _assets.TryAdd(cleanRoute, asset);
+                    _ = _assets.TryAdd(cleanRoute, asset);
                     return true;
                 }
             }
         }
 
         return false;
-    }
-
-    public IAsset AddBundle(IAsset asset) =>
-        AddBundle(asset.Route, asset.ContentType, [.. asset.SourceFiles]);
-
-    public IEnumerable<IAsset> AddBundle(IEnumerable<IAsset> assets)
-    {
-        var list = new List<IAsset>();
-
-        foreach (var asset in assets)
-        {
-            var ass = AddBundle(asset.Route, asset.ContentType, [.. asset.SourceFiles]);
-            list.Add(ass);
-        }
-
-        return list;
-    }
-
-    public IAsset AddBundle(string route, string contentType, params string[] sourceFiles)
-    {
-        if (sourceFiles.Length == 0)
-        {
-            throw new ArgumentException("At least one source file has to be specified", nameof(sourceFiles));
-        }
-
-        if (route.Contains('*') || route.Contains('[') && route.Contains('?'))
-        {
-            throw new ArgumentException($"The route \"{route}\" appears to be a globbing pattern which isn't supported for bundle routes.", nameof(route));
-        }
-
-        route = NormalizeRoute(route);
-
-        IAsset asset = new Asset(route, contentType, sourceFiles, _assetLogger);
-        _assets.TryAdd(route, asset);
-
-        return asset;
-    }
-
-
-    public IAsset AddAsset(string route, string contentType)
-    {
-        route = NormalizeRoute(route);
-
-        IAsset asset = new Asset(route, contentType, [], _assetLogger);
-        _assets.TryAdd(route, asset);
-
-        return asset;
-    }
-
-    public IEnumerable<IAsset> AddFiles(string contentType, params string[] sourceFiles)
-    {
-        if (string.IsNullOrEmpty(contentType))
-        {
-            throw new ArgumentException("A valid content type must be specified", nameof(contentType));
-        }
-
-        if (sourceFiles.Length == 0)
-        {
-            throw new ArgumentException("At least one source file has to be specified", nameof(sourceFiles));
-        }
-
-        var list = new List<IAsset>();
-
-        foreach (string file in sourceFiles)
-        {
-            var asset = new Asset(NormalizeRoute(file), contentType, [file], _assetLogger);
-            list.Add(asset);
-            _assets.TryAdd(asset.Route, asset);
-        }
-
-        return list;
-    }
-
-    public static string NormalizeRoute(string route)
-    {
-        string trimmedRoute = route.Trim();
-        string cleanRoute = trimmedRoute.StartsWith('/') || trimmedRoute.StartsWith('~')
-            ? $"/{route.Trim().TrimStart('~', '/')}"
-            : trimmedRoute;
-
-        int index = cleanRoute.IndexOfAny(['?', '#']);
-
-        if (index > -1)
-        {
-            cleanRoute = cleanRoute[..index];
-        }
-
-        if (!cleanRoute.StartsWith('/'))
-        {
-            cleanRoute = $"/{cleanRoute}";
-        }
-
-        return cleanRoute;
     }
 }
